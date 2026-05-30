0c0105b45d57732804991022223dee27663ee1a1
Tomas Vaner <TomasVanerG@gmail.com>
2026-05-30 22:37:13 +0300

    make microservice use BasicMCTS or AdvancedMCTS

--- src/microservice.cpp
+++ src/microservice.cpp
@@ -1,19 +1,37 @@
 #include "game/GameState.h"
-#include "ai/MCTS.h"
+#include "ai/AdvancedMCTS.h"
+#include "ai/BasicMCTS.h"
 #include "Listener.h"
 #include "utils.h"
 
 #include <iostream>
 
-int main() {
+
+int main(int argc, char* argv[]) {
 	auto const address = net::ip::make_address("0.0.0.0");
 	constexpr unsigned short port = 8080;
 
-	net::io_context ioc{1};
-	std::make_shared<Listener>(ioc, tcp::endpoint{address, port}, [](const std::string& request) {
-		try {
-			MCTS mcts(5000);
+	std::shared_ptr<MCTS> mcts;
 
+	NeuralNetwork neuralNetwork;
+	if (argc >= 3)
+	{
+		neuralNetwork.load(argv[1]);
+		mcts = std::make_unique<AdvancedMCTS>(&neuralNetwork, std::stoi(argv[2]));
+	}
+	else {
+		auto simulations = 5000;
+		if (argc >= 2)
+		{
+			simulations = std::stoi(argv[1]);
+		}
+
+		mcts = std::make_shared<BasicMCTS>(simulations);
+	}
+
+	net::io_context ioc{1};
+	std::make_shared<Listener>(ioc, tcp::endpoint{address, port}, [mcts](const std::string& request) {
+		try {
 			std::vector<std::string> requestParts = split(request, ',');
 			const char color = requestParts.at(0).at(0);
 			const std::string& board = requestParts.at(1);
@@ -24,8 +42,9 @@ int main() {
 			
 			GameState* gameState = GameState::newGame(color, board, previousBoards);
 
-			std::string response = std::to_string(gameState->getValidMoves()->at(mcts.getBestMove(gameState)));
+			std::string response = std::to_string(gameState->getValidMoves()->at(mcts->getBestMove(gameState)));
 
+			mcts->reset();
 			delete gameState;
 
 			return response;
